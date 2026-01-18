518197cf14d68538b07cd030072efc294cb5770c
Kanan-peoiks <kenanshukurzadee@gmail.com>
2026-01-18 11:22:50 +0400

    update department repository

---
+++ src/main/java/com/example/employeemanagementsystem/repository/DepartmentRepo.java
@@ -0,0 +1,13 @@
+package com.example.employeemanagementsystem.repository;
+
+import com.example.employeemanagementsystem.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepo extends JpaRepository<Department,Integer> {
+
+
+
+
+}
